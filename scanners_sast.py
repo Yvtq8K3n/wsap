080bc1705cd6393d3e881e026daab99a9ce84cf2
yvtq8k3n <yvtq8k3n@gmail.com>
2021-07-28 19:02:41 +0100

    Addint timestamp to tmp folder creation

--- scanners_sast.py
+++ scanners_sast.py
@@ -9,7 +9,7 @@ TMP_DIRECTORY =  os.path.dirname(__file__) + "/tmp/"
 
 class ScannersSast:
     def __init__(self, target_url, current_time):
-        PATH = TMP_DIRECTORY + urlparse(target_url).netloc + "_" + current_time
+        PATH = TMP_DIRECTORY + urlparse(target_url).netloc + "_" + current_time.strftime("%Y/%m/%d/")
 
         os.makedirs(PATH, exist_ok=True)
         print ('Launching InsiderCLI...')
